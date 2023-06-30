75987c397906778194bc16dab67d03cab8f5b05f
Chase Gaddis <cgaddis36@gmail.com>
2023-06-30 17:02:07 -0400

    add Sponsor type and Event type as fields

--- app/graphql/types/sponsor_event_type.rb
+++ app/graphql/types/sponsor_event_type.rb
@@ -5,6 +5,8 @@ module Types
     field :id, ID, null: false
     field :sponsor_id, Integer, null: false
     field :event_id, Integer, null: false
+    field :sponsor, Types::SponsorType
+    field :event, Types::EventType
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
