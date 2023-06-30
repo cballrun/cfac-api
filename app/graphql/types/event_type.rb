dee6c5e5376933eaddd3925ce2386ac073b60dbf
Chase Gaddis <cgaddis36@gmail.com>
2023-06-30 17:01:26 -0400

    add fields ArtistEvent, SponsorEvent & VendorEvent

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -7,6 +7,9 @@ module Types
     field :city, String
     field :state, String
     field :location, String
+    field :artist_events, [Types::ArtistEventType]
+    field :sponsor_events, [Types::SponsorEventType]
+    field :vendor_events, [Types::VendorEventType]
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :description, String
