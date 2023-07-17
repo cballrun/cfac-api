f1a2e96761f5f319bb19c2e729893d1479089313
Carter Ball <106977961+cballrun@users.noreply.github.com>
2023-07-17 18:49:48 -0400

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://192.168.1.68/graphql"
+    origins ""
 
     resource "*",
       headers: :any,
